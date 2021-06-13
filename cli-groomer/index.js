b5c2068bef181a6f85adc5e410c48a5bf432db97
gness1804 <graham.nessler@gmail.com>
2021-06-13 16:47:04 -0500

    Initial version of cli scaffolder. hardcoded data.

---
+++ cli-groomer/index.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+/**
+* cli-groomer
+* Something to groom aspects of a CLI.
+*/
---
+++ index.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+/* eslint-disable no-console */
+
+const path = require('path');
+const copy = require('copy-template-dir');
+
+const vars = {
+  name: 'cli-groomer',
+  description: 'Something to groom aspects of a CLI.',
+  version: '0.1.1',
+};
+const inDir = path.join(__dirname, 'template');
+const outDir = path.join(process.cwd(), vars.name);
+
+copy(inDir, outDir, vars, (err, createdFiles) => {
+  if (err) throw err;
+  console.log();
+  console.log(`Creating files in ./${vars.name}`);
+  createdFiles.forEach(filePath => {
+    const file = path.basename(filePath);
+    console.log(file);
+  })
+  console.info('done!')
+  console.log();
+})
