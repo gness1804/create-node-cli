83c3736c7430378772518644b6e2c49896730200
gness1804 <graham.nessler@gmail.com>
2021-06-25 16:30:04 -0500

    add name, description, version from package.json to init

--- utils/init.js
+++ utils/init.js
@@ -1,19 +1,27 @@
 import welcome from 'cli-welcome';
-// import pkg from '../package.json';
+
+// allows requiring .json files to avoid the error "TypeError [ERR_UNKNOWN_FILE_EXTENSION]: Unknown file extension ".json""
+// see https://stackoverflow.com/questions/60205891/import-json-extension-in-es6-node-js-throws-an-error, Mustafa Agha's answer
+import { createRequire } from 'module'; // Bring in the ability to create the 'require' method
+const require = createRequire(import.meta.url); // construct the require method
+const pkg = require('../package.json'); // use the require method
+
 import unhandled from 'cli-handle-unhandled';
 
 const func = () => {
   unhandled();
   welcome({
     title: 'create-node-cli',
-    tagLine: 'by Graham, sort of',
-    description: '',
-    version: '',
+    tagLine: `by ${pkg.author}`,
+    description: pkg.description,
+    version: pkg.version,
     bgColor: '#6cc24a',
     color: '#000',
     bold: true,
     clear: true,
   });
 };
+
+// TODO: add the updater.
 
 export default func;
